66cf4a528ee9c26ca9a0a933905c91b049676285
gchriswill <gchriswill@me.com>
2020-09-01 03:46:52 -0500

    Initial Commit

---
+++ Pokedex/ContentView.swift
@@ -0,0 +1,21 @@
+//
+//  ContentView.swift
+//  Pokedex
+//
+//  Created by Christopher Gonzalez on 9/1/20.
+//  Copyright © 2020 Christopher Gonzalez. All rights reserved.
+//
+
+import SwiftUI
+
+struct ContentView: View {
+    var body: some View {
+        Text("Hello, World!")
+    }
+}
+
+struct ContentView_Previews: PreviewProvider {
+    static var previews: some View {
+        ContentView()
+    }
+}
